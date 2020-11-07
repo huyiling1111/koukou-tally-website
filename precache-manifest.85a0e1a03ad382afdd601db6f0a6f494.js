26167f0ea073611dd052de46a90ae301f4599ec6
huyiling1111 <1052859577@qq.com>
2020-11-07 10:58:02 +0800

    deploy

---
+++ precache-manifest.85a0e1a03ad382afdd601db6f0a6f494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3e2d151f8bd1c1b4dc",
+    "url": "/koukou-tally-website/css/app.b3ada4d7.css"
+  },
+  {
+    "revision": "432e26f5e0c5cd0d419a",
+    "url": "/koukou-tally-website/css/chunk-vendors.8280f032.css"
+  },
+  {
+    "revision": "b8f9a7ed55e5173c1bb36c1b9ab22515",
+    "url": "/koukou-tally-website/index.html"
+  },
+  {
+    "revision": "9e3e2d151f8bd1c1b4dc",
+    "url": "/koukou-tally-website/js/app.b7f05306.js"
+  },
+  {
+    "revision": "432e26f5e0c5cd0d419a",
+    "url": "/koukou-tally-website/js/chunk-vendors.9fd6f166.js"
+  },
+  {
+    "revision": "d24e9b1395e7926fbe3c39285799d451",
+    "url": "/koukou-tally-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/koukou-tally-website/robots.txt"
+  }
+]);
